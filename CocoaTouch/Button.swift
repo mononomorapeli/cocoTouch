d03169cd59aa8ab11ba2efeb9e9125fe0cf49a05
mononomorapeli <minkymorapeli@gmail.com>
2023-05-22 14:29:20 +0200

    first commit

---
+++ CocoaTouch/Button.swift
@@ -0,0 +1,20 @@
+//
+//  Button.swift
+//  CocoaTouch
+//
+//  Created by DA MAC M1 122 on 2023/05/19.
+//
+
+import UIKit
+
+class Button: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
